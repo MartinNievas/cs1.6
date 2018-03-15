60821d5ac991356cb2f6fab560a4a68a4239ef7b
Iñaki Malerba <inaki@satellogic.com>
2018-03-15 14:20:14 -0300

    Fix permission problem on xhost

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
+#! /bin/bash
+function teardown {
+    xhost -local:root
+}
+
+trap teardown EXIT
+xhost +local:root
+
 docker run -it --rm \
     --env="DISPLAY" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
